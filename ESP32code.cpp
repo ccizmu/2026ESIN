3456d9020bfa09f3331ffe16f12ce17e9b68f037
ccizmu <143674974+ccizmu@users.noreply.github.com>
2026-06-12 17:33:07 +0800

    Update ESP32code.cpp

--- ESP32code.cpp
+++ ESP32code.cpp
@@ -52,6 +52,9 @@ const int LED_BLUE  = 4;
 // =========================
 int lastDirection = 0;
 
+unsigned long turnStartTime = 0;
+bool turning = false;
+
 void setup()
 {
   Serial.begin(115200);
@@ -158,7 +161,23 @@ setASLAutonomous();
 if (s5 == 0)
 {
     lastDirection = -1;
+
+    if (!turning)
+    {
+        turning = true;
+        turnStartTime = millis();
+    }
+
     spinLeft();
+
+    // 超過0.7秒還沒離開
+    if (millis() - turnStartTime > 700)
+    {
+        move(100,100);
+        delay(150);
+
+        turning = false;
+    }
 }
 
 // =========================
@@ -167,7 +186,22 @@ if (s5 == 0)
 else if (s1 == 0)
 {
     lastDirection = 1;
+
+    if (!turning)
+    {
+        turning = true;
+        turnStartTime = millis();
+    }
+
     spinRight();
+
+    if (millis() - turnStartTime > 700)
+    {
+        move(100,100);
+        delay(150);
+
+        turning = false;
+    }
 }
 
 // =========================
